4c0cfb3d26d2ab2845e403b2b103ec089f364266
kwh8057 <kdk7030@gmail.com>
2017-10-27 16:12:13 +0900

    Update MapsActivity.java

--- app/src/main/java/com/example/song/global/MapsActivity.java
+++ app/src/main/java/com/example/song/global/MapsActivity.java
@@ -1,9 +1,14 @@
-package com.example.song.global;
+package com.example.user.korearoad;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -32,44 +37,26 @@ public class MapsActivity extends FragmentActivity
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
 
-        LatLng kim_musiem = new LatLng(41.138036, 69.308668);
+        LatLng kim_museum = new LatLng(41.138036, 69.308668);
+        mMap.addMarker(new MarkerOptions().position(kim_museum).title("김병화 박물관")).showInfoWindow();
 
-        mMap.addMarker(new MarkerOptions().position(kim_musiem).title("김병화 박물관")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.museum))
-                .snippet("김병화 박물관이란? 블라블라"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kim_museum, 12)); // 줌 : 숫자가 커질수록 확대
 
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kim_musiem, 12)); // 줌 : 숫자가 커질수록 확대
-
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener(){
             @Override
-            public boolean onMarkerClick(Marker m) {
-                m.showInfoWindow();
-                mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener(){
-                    @Override
-                    public void onInfoWindowClick(Marker marker) {
-                        turnTableNavigation(marker);
-                    }
-                });
-                return true;
+            public void onInfoWindowClick(Marker marker) {
+                turnTableNavigation(marker);
             }
         });
-
     }
+
+
 
     public void turnTableNavigation(Marker m)
     {
@@ -87,4 +74,6 @@ public class MapsActivity extends FragmentActivity
     @Override
     public void onInfoWindowClick(Marker marker) {
     }
+
+
 }
