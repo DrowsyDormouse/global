25df4672c22bd014db7581c8753f8c3b4d0801db
kwh8057 <kdk7030@gmail.com>
2017-11-07 18:39:57 +0900

    마커 추가

--- app/src/main/java/com/example/song/global/MapsActivity.java
+++ app/src/main/java/com/example/song/global/MapsActivity.java
@@ -57,10 +57,28 @@ public class MapsActivity extends FragmentActivity
         Resources res = getResources();
 
         LatLng latLng[] = new LatLng[] {
-                new LatLng(41.138036, 69.308668) // 김병화 박물관 tag 01
-                ,new LatLng(39.660730, 66.980343) // 비비하눔 사원 tag 02
-                ,new LatLng(41.489135, 69.586206) // 치르치크 tag 00
+                new LatLng(53.242399, 50.218703) // 사마라 0
+                ,new LatLng(41.300570, 69.240908) // 타슈켄트 1
+                ,new LatLng(43.232465, 76.845968) // 알마티 2
+                ,new LatLng(45.242487, 77.972222) // 우슈토베 3
+                ,new LatLng(44.844705, 65.484732) // 크질로르다 4
+                ,new LatLng(42.878289, 74.556483) // 비슈케크 5
+                ,new LatLng(55.007475, 82.957625) // 노보시비르스크 6
+                ,new LatLng(56.007275, 92.892656) // 크라스노야르스크 7
+                ,new LatLng(52.336070, 104.322924) // 이르쿠츠크 8
+                ,new LatLng(52.059708, 113.474373) // 치타 9
+                //,new LatLng(53.962738, 123.769936) // 아무르스카야 9.1
+                //,new LatLng(49.102856, 131.108803) // 오블루치예 9.2
+                ,new LatLng(48.516396, 135.105021) // 하바롭스크 10
+                ,new LatLng(43.801456, 131.965403) // 우수리스크 11
+                ,new LatLng(43.124270, 131.917708) // 블라디보스토크 12
+
+                ,new LatLng(39.660730, 66.980343) // 비비하눔 사원 tag 13
+                ,new LatLng(41.138036, 69.308668) // 김병화 박물관 tag 14
+                ,new LatLng(41.489135, 69.586206) // 치르치크 tag 15
         };
+        LatLng amur = new LatLng(53.962738, 123.769936);
+        LatLng oblu = new LatLng(49.102856, 131.108803);
 
         for (int i = 0; i < latLng.length; i++) {
 
@@ -85,13 +103,26 @@ public class MapsActivity extends FragmentActivity
         }
 
         PolylineOptions rectOptions = new PolylineOptions()
-                .add(latLng[1])
                 .add(latLng[0])
-                .add(latLng[2]);
+                .add(latLng[4])
+                .add(latLng[1])
+                .add(latLng[15])
+                .add(latLng[5])
+                .add(latLng[2])
+                .add(latLng[3])
+                .add(latLng[6])
+                .add(latLng[7])
+                .add(latLng[8])
+                .add(latLng[9])
+                .add(amur)
+                .add(oblu)
+                .add(latLng[10])
+                .add(latLng[11])
+                .add(latLng[12]);
         Polyline polyline = mMap.addPolyline(rectOptions);
         polyline.setColor(0xffff0000);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng[1], 8)); // 줌 : 숫자가 커질수록 확대
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng[0], 10)); // 줌 : 숫자가 커질수록 확대
 
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
